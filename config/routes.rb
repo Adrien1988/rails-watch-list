42c04da9965daa2c34fbeaeafec6d977c6177605
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2021-05-13 17:03:08 +0200

    back complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # faire une route home page
+  # faire une route list avec index, show, new, create (resources: lists)
+  resources :lists, only: [:index, :new, :create, :show] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
