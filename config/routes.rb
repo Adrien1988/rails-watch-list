10573917a0327b986ba6cb14d3ded7e5058ba8aa
Adrien Fauquembergue <adrien_fauque@hotmail.fr>
2021-05-14 12:51:52 +0200

    active-storage

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # faire une route home page
   # faire une route list avec index, show, new, create (resources: lists)
-  root to: "lists#index"
-  resources :lists, except: :index
-  resources :lists, only: [:new, :create, :show] do
+  resources :lists, only: [:index, :new, :create, :show] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
